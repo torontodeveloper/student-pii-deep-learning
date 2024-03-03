d490a7d28b9511863e1d35ecd07d568e1c123fed
Kakolla <kevin.kakolla@fujitsu.com>
2024-03-02 19:50:49 -0500

    initial scaffolding project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To develop a model that detects personally identifiable information (PII) in student writing. Your efforts to automate the detection and removal of PII from educational data will lower the cost of releasing educational datasets. This will support learning science research and the development of educational tools.',
+    author='University Of Toronto',
+    license='MIT',
+)
